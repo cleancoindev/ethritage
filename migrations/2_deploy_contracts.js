1fa268dc614d74ad3636e69ff595a245ce1de74a
Dennison Bertram <dennison@dennisonbertram.com>
2018-11-14 12:33:38 -0500

    Added Ethritage Token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,15 @@
 var SimpleStorage = artifacts.require("SimpleStorage");
 var TutorialToken = artifacts.require("TutorialToken");
 var ComplexStorage = artifacts.require("ComplexStorage");
+var ethritageToken = artifacts.require("ethritageToken");
+
+const name = "ethritage";
+const symbol = "ERTG";
+
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
   deployer.deploy(TutorialToken);
   deployer.deploy(ComplexStorage);
+  deployer.deploy(ethritageToken, name, symbol);
 };
