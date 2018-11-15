4b1f20c7e8f301aec1296bb74d4babfda39e6c93
Dennison Bertram <dennison@dennisonbertram.com>
2018-11-15 16:00:08 -0500

    added PickleDo ERC721 Server interface

---
+++ server/erc721-Interface.js
@@ -0,0 +1,418 @@
+"use strict";
+
+class tokenInterface {
+  constructor(gas, keypair, contractInstance, web3Plus) {
+    this.txLog = {};
+    this.txIndex = [];
+
+    const { Web3, HDWalletProvider, Tx, server, wsServer } = web3Plus;
+    this.Web3 = Web3;
+    this.HDWalletProvider = HDWalletProvider;
+    this.Tx = Tx;
+    this.server = server;
+    this.wsServer = wsServer;
+
+    this.provider = new this.HDWalletProvider(this.memonic, this.server);
+    this.web3 = new this.Web3(this.provider);
+
+    this.eventPRovider = new this.Web3.providers.WebsocketProvider(this.wsServer);
+    this.web3.setProvider(this.eventPRovider);
+
+
+    const { price, limit } = gas;
+    this.gasPrice = this.web3.utils.toHex(price);
+    this.gasLimit = this.web3.utils.toHex(limit);
+
+    const { myAddress, privateKey, memonic } = keypair;
+    this.myAddress = myAddress;
+    this.privateKey = Buffer.from(privateKey, "hex");
+    this.memonic = memonic;
+
+    const { contractABI, contractAddress, contractBuild } = contractInstance;
+    this.contractABI = contractABI;
+    this.contractAddress = contractAddress;
+    this.contractBuild = contractBuild;
+    this.contract = new this.web3.eth.Contract(
+      this.contractABI,
+      this.contractAddress
+    );
+
+    //this.subscribeAllEventsTest();
+  }
+
+  subscribeToAllEvents() {
+
+    return this.web3.eth.subscribe;
+
+    // this.web3.eth.subscribe('newBlockHeaders', (error, blockHeader) => {
+    //   if (error) console.log(error)
+    //   console.log("Here is blockheader", blockHeader);
+    // })
+  }
+
+  subscribeToContractEvents() {
+
+    return this.contract.events;
+
+    // this.contract.events.allEvents({
+    //   fromBlock: 0
+    // }, function (error, event) {
+    //   if (error) console.log(error)
+    //   console.log("Block Event", event)
+    // })
+  }
+
+  getMostRecentTx() {
+    return this.txLog;
+  }
+
+  getTxHistory() {
+    return this.txIndex;
+  }
+
+  async getTxCount() {
+    const count = await this.web3.eth.getTransactionCount(this.myAddress);
+    return count;
+  }
+
+  _buildTransaction(from, to, method, count, value = "0x0") {
+    //this needs to be put into configuration object
+    const gasPrice = this.gasPrice;
+    const gasLimit = this.gasLimit;
+    const nonce = this.web3.utils.toHex(count);
+
+    const rawTransaction = this._rawTxbuilder(
+      from,
+      gasPrice,
+      gasLimit,
+      to,
+      value,
+      method,
+      nonce
+    );
+
+    return rawTransaction;
+  }
+
+  _rawTxbuilder(from, gasPrice, gasLimit, to, value, data, nonce) {
+    const rawTransaction = {
+      from,
+      gasPrice,
+      gasLimit,
+      to,
+      value,
+      data,
+      nonce
+    };
+
+    const transaction = new this.Tx(rawTransaction);
+
+    return transaction;
+  }
+
+  _signTransaction(transaction) {
+    transaction.sign(this.privateKey);
+
+    return transaction;
+  }
+
+  async _sendTransaction(transaction) {
+    return this.web3.eth.sendSignedTransaction(
+      "0x" + transaction.serialize().toString("hex")
+    );
+
+  }
+
+  async _mintWithTokenURI(addressTo, tokenId, URIString) {
+    const count = await this.getTxCount();
+
+    const contractMethod = this.contract.methods
+      .mintWithTokenURI(addressTo, tokenId, URIString)
+      .encodeABI();
+
+    const rawTransaction = this._buildTransaction(
+      addressTo,
+      this.contractAddress,
+      contractMethod,
+      count
+    );
+
+    const transaction = this._signTransaction(rawTransaction, this.privateKey);
+    return this._sendTransaction(transaction);
+  }
+
+  async addMinter(address) {
+    const count = await this.getTxCount();
+
+    const contractMethod = this.contract.methods.addMinter(address).encodeABI();
+
+    const rawTransaction = this._buildTransaction(
+      this.myAddress,
+      this.contractAddress,
+      contractMethod,
+      count
+    );
+
+    const transaction = this._signTransaction(rawTransaction, this.privateKey);
+    return this._sendTransaction(transaction);
+    
+  }
+
+  async approve(addressTo, tokenId) {
+    const count = await this.getTxCount();
+
+    const contractMethod = this.contract.methods
+      .approve(addressTo, tokenId)
+      .encodeABI();
+
+    const rawTransaction = this._buildTransaction(
+      this.myAddress,
+      this.contractAddress,
+      contractMethod,
+      count
+    );
+
+    const transaction = this._signTransaction(rawTransaction, this.privateKey);
+    return this._sendTransaction(transaction);
+
+  }
+
+  async mint(addressTo, tokenId) {}
+
+  async mintToken(URI, tokenId = null) {
+    if (!tokenId) {
+      tokenId = await this.getTxCount();
+    }
+
+    return this._mintWithTokenURI(this.myAddress, tokenId, URI);
+
+  }
+
+  async renounceMinter() {
+    const count = await this.getTxCount();
+
+    const contractMethod = this.contract.methods
+      .renounceMinter()
+      .encodeABI();
+
+    const rawTransaction = this._buildTransaction(
+      this.myAddress,
+      this.contractAddress,
+      contractMethod,
+      count
+    );
+
+    const transaction = this._signTransaction(rawTransaction, this.privateKey);
+    return this._sendTransaction(transaction);
+  }
+
+  async safeTransferFrom(addressFrom, addressTo, tokenId, _data = "0x0") {
+    const count = await this.getTxCount();
+
+    const contractMethod = this.contract.methods
+      .safeTransferFrom(addressFrom, addressTo, tokenId, _data)
+      .encodeABI();
+
+    const rawTransaction = this._buildTransaction(
+      this.myAddress,
+      this.contractAddress,
+      contractMethod,
+      count
+    );
+
+    const transaction = this._signTransaction(rawTransaction, this.privateKey);
+    return this._sendTransaction(transaction);
+  }
+
+  async setApprovalForAll(addressTo, boolApproved) {
+    const count = await this.getTxCount();
+
+    const contractMethod = this.contract.methods
+      .setApprovalForAll(addressTo, boolApproved)
+      .encodeABI();
+
+    const rawTransaction = this._buildTransaction(
+      this.myAddress,
+      this.contractAddress,
+      contractMethod,
+      count
+    );
+
+    const transaction = this._signTransaction(rawTransaction, this.privateKey);
+    return this._sendTransaction(transaction);
+  }
+
+  async transferFrom(addressFrom, addressTo, tokenId) {
+    const count = await this.getTxCount();
+
+    const contractMethod = this.contract.methods
+      .transferFrom(addressFrom, addressTo, tokenId)
+      .encodeABI();
+
+    const rawTransaction = this._buildTransaction(
+      this.myAddress,
+      this.contractAddress,
+      contractMethod,
+      count
+    );
+
+    const transaction = this._signTransaction(rawTransaction, this.privateKey);
+    return this._sendTransaction(transaction);
+    
+  }
+
+  async balanceOf(address) {
+    try {
+      let response = await this.contract.methods.balanceOf(address).call({from: this.myAddress});
+      return response;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async getApproved(tokenId) {
+    try {
+      let response = await this.contract.methods.getApproved(tokenId).call({from: this.myAddress});
+      return response;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async isApprovedForAll(addressOwner, addressOperator) {
+    try {
+      let response = await this.contract.methods.isApprovedForAll(addressOwner, addressOperator).call({from: this.myAddress});
+      return response;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async isMinter(addressMinter) {
+    try {
+      let response = await this.contract.methods.isMinter(addressMinter).call({from: this.myAddress});
+      return response;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async name() {
+    try {
+      let response = await this.contract.methods.name().call({from: this.myAddress});
+      return response;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async ownerOf(tokenId) {
+    try {
+      let response = await this.contract.methods.ownerOf(tokenId).call({from: this.myaddress});
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async supportsInterFace(interfaceIdBytes4) {
+    try {
+      let response = await this.contract.methods.supportsInterface(interfaceIdBytes4).call({from: this.myAddress});
+      return response
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async symbol() {
+    try {
+      let symbol = await this.contract.methods.symbol().call({from: this.myAddress});
+      return symbol;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async tokenByIndex(index) {
+    try {
+      let tokenByIndex = await this.contract.methods.tokenByIndex(index).call({from: this.myAddress});
+      return tokenByIndex;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async tokenOfOwnerByIndex(addressOwner, index) {
+    try {
+      let tokenOfOwner = await this.contract.methods.tokenOfOwnerByIndex(addressOwner, index).call({from: this.myAddress});
+      return tokenOfOwner;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async tokenURI(tokenId) {
+    try {
+      let tokenURI = await this.contract.methods.tokenURI(tokenId).call({from: this.myAddress});
+      return tokenURI;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async totalSupply() {
+    try {
+      let supply = await this.contract.methods.totalSupply().call({from: this.myAddress});
+      //console.log("This is the total count: ". supply);
+      return supply;
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  //Just for reference at this point
+  async deploy() {
+    console.log("Using Module Class Version");
+
+    const balanceOf = await this.contract.methods
+      .balanceOf(this.myAddress)
+      .call();
+    //const accounts = await this.web3.eth.getAccounts();
+    let totalCount = await this.contract.methods.totalSupply().call({from: this.myAddress});
+
+    console.log("Total Count is: ", totalCount);
+    const count = await this.getTxCount();
+    console.log("Accounts: ", this.accounts);
+    console.log("Count: ", count);
+
+    const contractMethod = this.contract.methods
+      .mintWithTokenURI(this.myAddress, count, "This is token 1")
+      .encodeABI();
+
+    const rawTransaction = this._buildTransaction(
+      this.myAddress,
+      this.contractAddress,
+      contractMethod,
+      count
+    );
+
+    const transaction = this._signTransaction(rawTransaction, this.privateKey);
+
+    this._sendTransaction(transaction);
+
+    const isMinter = await this.contract.methods
+      .isMinter(this.myAddress)
+      .call();
+
+    console.log("Name: ", isMinter, " Balance: ", balanceOf);
+
+    return balanceOf;
+  }
+
+  static connect(values) {
+    return new tokenInterface(values);
+  }
+}
+
+module.exports = tokenInterface;
---
+++ test/tokenInterface.test.js
@@ -0,0 +1,214 @@
+"use strict";
+
+const assert = require("assert");
+const uuidv4 = require("uuid/v4");
+const TokenInterface = require("../server/erc721-Interface");
+
+const Web3 = require("web3");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Tx = require("ethereumjs-tx");
+const server = "HTTP://127.0.0.1:7545";
+const wsServer = "ws://localhost:7545";
+const networkId = 5777;
+
+const ganacheAccountZero = "0x2ca4488037250f9453032aa8de9be5786c5c178b";
+
+describe("tokenInterface", () => {
+  //Accessible for all the tests
+  let tokenId;
+  let tokenURI;
+
+  let mintedtokens = {};
+
+  const gas = {
+    price: 20 * 1e8,
+    limit: 2100000
+  };
+
+  const keypair = {
+    memonic:
+      "detail august fragile luggage coyote home trap veteran witness result feed blade",
+    myAddress: "0x2cA4488037250f9453032aA8dE9bE5786c5c178B",
+    privateKey:
+      "62b8292bc6e27d594b7bf4f71bcb79c85e26cd506704c3f14d21ed1e17cfd9d3"
+  };
+
+  const contractInstance = {
+    contractBuild: require("../build/contracts/testToken"),
+    contractABI: require("../build/contracts/testToken").abi,
+    contractAddress: require("../build/contracts/testToken").networks[networkId]
+      .address
+  };
+
+  const web3Plus = {
+    Web3,
+    HDWalletProvider,
+    Tx,
+    server,
+    wsServer
+  };
+
+  const tokenInterface = new TokenInterface(
+    gas,
+    keypair,
+    contractInstance,
+    web3Plus
+  );
+
+  beforeEach(async () => {
+    // let events = await tokenInterface.subscribeToAllEvents();
+    // events('newBlockHeaders', (error, blockHeader) => {
+    //   if (error) console.log(error)
+    //   console.log("Here is blockheader", blockHeader);
+    // })
+  });
+
+  it("Should mint a token...", async () => {
+    tokenURI = uuidv4();
+
+    let events = await tokenInterface.subscribeToContractEvents();
+    events.Transfer(
+      {
+        fromBlock: 0
+      },
+      function(error, event) {
+        if (error) {
+          console.log(error);
+          assert.equal(event.event, "Transfer");
+        }
+
+        tokenId = event.returnValues.tokenId;
+
+        assert.equal(event.event, "Transfer");
+      }
+    );
+
+    const result = await tokenInterface.mintToken(tokenURI);
+  }).timeout(5000);
+
+  it("Should return a Token URI...", async () => {
+    let response = await tokenInterface.tokenURI(tokenId);
+
+    assert.equal(tokenURI, response);
+  });
+
+  it("Should return token of owner by index...", async () => {
+    let ownedTokens = [];
+    const address = ganacheAccountZero;
+    const index = 0;
+    let tokensOwned = await tokenInterface.balanceOf(address);
+
+    let success = true;
+
+    for (let x = 0; x < tokensOwned; x++) {
+      ownedTokens.push(await tokenInterface.tokenOfOwnerByIndex(address, x));
+    }
+
+    for (let x = 0; x < ownedTokens.length; x++) {
+      let owner = await tokenInterface.ownerOf(ownedTokens[x]);
+      if (owner.toLowerCase() !== address) {
+        success = false;
+      }
+    }
+
+    assert.ok(success);
+  });
+
+  xit("Should return the symbol 'tt'...", async () => {
+    let response = await tokenInterface.symbol();
+    assert.equal(response, "tt");
+  });
+
+  xit("Should respond false for supporting uknown interface...", async () => {
+    const code = "0x032";
+    let response = await tokenInterface.supportsInterFace(code);
+    assert.ok(!response);
+  });
+
+  xit("Should respond with the owner address...", async () => {
+    const expectedAddress = ganacheAccountZero;
+    const token = 645;
+    let response = await tokenInterface.ownerOf(token);
+    assert.equal(response.toLowerCase(), expectedAddress);
+  });
+
+  xit("Should respond with the token Name...", async () => {
+    const expectedName = "testToken";
+    let response = await tokenInterface.name();
+    assert.equal(response, expectedName);
+  });
+
+  xit("Should return status of isMinter for an address...", async () => {
+    const address = ganacheAccountZero;
+    let response = await tokenInterface.isMinter(address);
+    assert.ok(response);
+  });
+
+  xit("Should return false for an unaproved isApprovedForAll operator...", async () => {
+    const addressOwner = ganacheAccountZero;
+    const addressOperator = "0xBc8a2A1Cb9a192bDb2A167d4d1807F4895d1C65B";
+    let response = await tokenInterface.isApprovedForAll(
+      addressOwner,
+      addressOperator
+    );
+    assert.ok(!response);
+  });
+
+  xit("Should return no one is approved for getApproved...", async () => {
+    const tokenId = 645;
+    const nullAddress = "0x0000000000000000000000000000000000000000";
+    let response = await tokenInterface.getApproved(tokenId);
+    assert.equal(response, nullAddress);
+  });
+
+  xit("Should return the balance of an address...", async () => {
+    const address = ganacheAccountZero;
+    let response = await tokenInterface.balanceOf(address);
+    assert.ok(response);
+  });
+
+  xit("Should Transfer a token from and address to an address...", async () => {
+    const addressFrom = ganacheAccountZero;
+    const addressTo = "0x4A3EAeA9f76E26084520926EeC8fCd90d1F08a69";
+    const tokenID = 5;
+
+    let response = await tokenInterface.transferFrom(
+      addressFrom,
+      addressTo,
+      tokenID
+    );
+    //console.log("Response: ", response);
+    assert.equal(response.logs[0].type, "mined");
+  });
+
+  xit("Should approve the transfer of all tokens...", async () => {
+    const addressTo = "0x4A3EAeA9f76E26084520926EeC8fCd90d1F08a69";
+    const approval = true;
+
+    let response = await tokenInterface.setApprovalForAll(addressTo, approval);
+    //console.log("Response: ", response);
+    assert.equal(response.logs[0].type, "mined");
+  });
+
+  xit("Should safeTransfer a token from and address to an address...", async () => {
+    const addressFrom = ganacheAccountZero;
+    const addressTo = "0x4A3EAeA9f76E26084520926EeC8fCd90d1F08a69";
+    const tokenID = 809;
+
+    let response = await tokenInterface.safeTransferFrom(
+      addressFrom,
+      addressTo,
+      tokenID
+    );
+    //console.log("Response: ", response);
+    assert.equal(response.logs[0].type, "mined");
+  });
+
+  xit("Renounce Minter...");
+
+  // it('Should add a minter', async (done) => {
+  //     const result = await tokenInterface.addMinter("0x85A7bAC4da4Bc90820339759E73bee84D1D28c3E");
+  //     setTimeout(done, 4500);
+  //     assert.ok(result);
+  // })
+});
