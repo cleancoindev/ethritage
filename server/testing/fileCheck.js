2eab9081c386d86434ec7ce524f8b9aa08a2ac67
Dennison Bertram <dennison@dennisonbertram.com>
2018-11-16 19:13:47 -0500

    refactoring

--- server/testing/fileCheck.js
+++ server/testing/fileCheck.js
@@ -1,67 +1,59 @@
+const { contractInstance } = require("./contractInstance");
+const keypair = require("../../secrets");
+
 const chokidar = require("chokidar");
 const fs = require("fs");
-const path = require("path");
-const IPFS = require('ipfs')
-const IPFSnode = new IPFS()
+const IPFS = require("ipfs");
+const IPFSnode = new IPFS();
+const Parser = require("exif-parser");
 
+//------------ // Image Management // ------------//
+const Jimp = require("jimp");
+Jimp.RESIZE_HERMITE;
 
-IPFSnode.on('ready', () => {
-  console.log("IPFS READY: ");
-})
+const qualitySettings = {
+  quality: 70,
+  resize: 250
+}
 
-///Token Interface
+//------------ // Token Management // ------------//
 const TokenInterface = require("../erc721-Interface");
 
+//------------ // Web3 Interface // ------------//
 const Web3 = require("web3");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const Tx = require("ethereumjs-tx");
 const server = "HTTP://127.0.0.1:7545";
 const wsServer = "ws://localhost:7545";
-const networkId = 5777;
-//const ganacheAccountZero = "0x2ca4488037250f9453032aa8de9be5786c5c178b";
-
-
-
 const gas = {
-    price: 20 * 1e8,
-    limit: 2100000
-  };
+  price: 20 * 1e8,
+  limit: 2100000
+};
+const web3Plus = {
+  Web3,
+  HDWalletProvider,
+  Tx,
+  server,
+  wsServer
+};
+const tokenInterface = new TokenInterface(
+  gas,
+  keypair,
+  contractInstance,
+  web3Plus
+);
 
-  const keypair = require('../../secrets');
+//------------ // Folder To Watch // ------------//
 
-  const contractInstance = {
-    contractBuild: require("../../build/contracts/ethritageToken.json"),
-    contractABI: require("../../build/contracts/ethritageToken.json").abi,
-    contractAddress: require("../../build/contracts/ethritageToken.json").networks[networkId]
-      .address
-  };
+const watchedFolder = "./testFolder";
 
-  const web3Plus = {
-    Web3,
-    HDWalletProvider,
-    Tx,
-    server,
-    wsServer
-  };
+//------------ // Initialize IPFS Node // ------------//
 
-  const tokenInterface = new TokenInterface(
-    gas,
-    keypair,
-    contractInstance,
-    web3Plus
-  );
+IPFSnode.on("ready", () => {
+  console.log("IPFS READY: ");
+});
 
-
-
-const Jimp = require("jimp");
-Jimp.RESIZE_HERMITE;
-
-const Parser = require("exif-parser");
-
-
-let files = [];
-
-const watcher = chokidar.watch("./testFolder", {
+const watcher = chokidar.watch(watchedFolder, {
   ignored: /(^|[\/\\])\../,
   persistent: true
 });
@@ -71,121 +63,109 @@ const watcher = chokidar.watch("./testFolder", {
 const log = console.log.bind(console);
 
 // Add event listeners.
-watcher
-  .on("add", thisPath => log(`File ${thisPath} has been added`))
-  .on("change", thisPath => log(`File ${thisPath} has been changed`))
-  .on("unlink", thisPath => log(`File ${thisPath} has been removed`));
+watcherGeneral();
 
-watcher.on("add", async filePath => {
-  console.log("The FilePath is: ", filePath);
-  const jpegData = fs.readFileSync(filePath);
-  //const finalHash = await md5(jpegData);
-  const parser = Parser.create(jpegData);
+watchFile();
 
-  const image = await Jimp.read(jpegData);
-
-  const image2 = image.clone();
-  image2.quality(70);
-  image2.resize(250, Jimp.AUTO);
-
-  let result;
-
-  const tokenObject = {
-    hiRes: "",
-    lowRes: "",
-    exif: {}
-  }
-  const ipfsImage1 = await IPFSnode.files.add(jpegData);
-
-  let image2buffer = await image2.getBufferAsync(Jimp.MIME_JPEG);
-  const ipfsImage2 = await IPFSnode.files.add(image2buffer);
-
-  console.log("IPFS -> High: ", ipfsImage1);
-  console.log("IPFS -> Low: ", ipfsImage2);
-
-  tokenObject.hiRes = ipfsImage1;
-  tokenObject.lowRes = ipfsImage2;
-
-let finalHash;
-
-
-
+const mkdirSync = function(dirPath) {
   try {
-    result = await parser.parse();
-    //console.log("Parsed Data is: ", result);
-
-    tokenObject.exif = result;
-    console.log("");
-    console.log("Object is: ", tokenObject);
-  
-    finalHash = await IPFSnode.files.add(Buffer.from(JSON.stringify(tokenObject), 'utf8'));
-    finalHash = finalHash[0].hash;
-
-    console.log("Final Hash Is: ", finalHash);
-
+    fs.mkdirSync(dirPath);
   } catch (err) {
-    // got invalid data, handle error
-    console.log(err);
+    if (err.code !== "EEXIST") throw err;
   }
+};
 
-  let filePathArray = filePath.split("/");
-  console.log("FILE PATH ARRAY", filePathArray);
-  let fileName = filePathArray[1].split(".");
-  let newFileName = `${fileName[0]}_${finalHash}.${fileName[1]}`;
+const mintIt = async uri => {
+  let events = await tokenInterface.subscribeToContractEvents();
+  events.Transfer(
+    {
+      fromBlock: 0
+    },
+    function(error, event) {
+      if (error) {
+        console.log(error);
+      }
 
-  mkdirSync(`./finished/${finalHash}/`);
+      let tokenId = event.returnValues.tokenId;
 
-  fs.rename(filePath, `./finished/${finalHash}/${fileName[0]}_${finalHash}.${image.getExtension()}`, function(
-    err
-  ) {
-    if (err) throw err;
-    console.log("Move complete.");
-  });
-
-  fs.writeFile(
-    `./finished/${finalHash}/${fileName[0]}_${finalHash}.txt`,
-    JSON.stringify(tokenObject),
-    err => {
-      console.log(err);
-    }   
+      console.log("The Event for Token: ", event.event);
+      console.log("The Token id: ", tokenId);
+    }
   );
 
+ await tokenInterface.mintToken(uri);
 
-let smallfile = `./finished/${finalHash}/${fileName[0]}_small_${finalHash}.` + image2.getExtension();
-image2.write(smallfile);
+};
 
+function watchFile() {
+  watcher.on("add", async (filePath) => {
 
-await mintIt(finalHash);
+    const instance = {};
 
-});
+    //------------ // Create a buffer from the added File // ------------//
+    const _jpegData = fs.readFileSync(filePath);
+    instance.hiResImage = _jpegData;
 
+    //------------ // Parse the Image for Exif Data // ------------//
+    const parser = Parser.create(instance.jpegData);
+    const _imageExifData = await parser.parse();
+    instance.imageExifData = _imageExifData;
 
-const mkdirSync = function (dirPath) {
-    try {
-      fs.mkdirSync(dirPath)
-    } catch (err) {
-      if (err.code !== 'EEXIST') throw err
-    }
-  }
+    //------------ // Clone Image // ------------//
+    const image_temp = await Jimp.read(instance.hiResImage);
+    const imageThumbnail = image_temp.clone();
+    
+    //------------ // Resize Image // ------------//
+    imageThumbnail.quality(qualitySettings.quality);
+    imageThumbnail.resize(qualitySettings.resize, Jimp.AUTO);
 
-const mintIt = async (uri) => {
-    let events = await tokenInterface.subscribeToContractEvents();
-    events.Transfer(
-      {
-        fromBlock: 0
-      },
-      function(error, event) {
-        if (error) {
-          console.log(error);
-        }
+    //------------ // Convert Thumbnail Image into a Buffer // ------------//
+    const _imageThumbnailbuffer = await imageThumbnail.getBufferAsync(Jimp.MIME_JPEG);
+    instance.lowResImage = _imageThumbnailbuffer;
 
-        let tokenId = event.returnValues.tokenId;
+    //------------ // Hash individual Images // ------------//
+    const tokenObject = {
+      hiRes: "",
+      lowRes: "",
+      exif: {}
+    };
+    tokenObject.exif = instance.imageExifData;
 
-        console.log("The Event for Token: ", event.event);
-        console.log("The Token id: ", tokenId);
-      }
-    );
+    const ipfsImageHiResHash = await IPFSnode.files.add(instance.jpegData);
+    const ipfsImageThumbnailHash = await IPFSnode.files.add(instance.lowResImage);
 
-    const result = await tokenInterface.mintToken(uri);
-    //console.log("Result is: ", result);
+    console.log("IPFS -> High: ", ipfsImageHiResHash);
+    console.log("IPFS -> Low: ", ipfsImageThumbnailHash);
+
+    tokenObject.hiRes = ipfsImageHiResHash;
+    tokenObject.lowRes = ipfsImageThumbnailHash;
+
+    const _finalHash = await IPFSnode.files.add(Buffer.from(JSON.stringify(tokenObject), "utf8"));
+    instance.finalHash = _finalHash[0].hash;
+
+    const filePathArray = filePath.split("/");
+    
+    const fileName = filePathArray[1].split(".");
+    const newFileName = `${fileName[0]}_${instance.finalHash}.${fileName[1]}`;
+    mkdirSync(`./finished/${instance.finalHash}/`);
+    fs.rename(filePath, `./finished/${instance.finalHash}/${fileName[0]}_${instance.finalHash}.${image.getExtension()}`, function (err) {
+      if (err)
+        throw err;
+      console.log("Move complete.");
+    });
+    fs.writeFile(`./finished/${instance.finalHash}/${fileName[0]}_${instance.finalHash}.txt`, JSON.stringify(tokenObject), err => {
+      console.log(err);
+    });
+    let smallfile = `./finished/${instance.finalHash}/${fileName[0]}_small_${instance.finalHash}.` +
+      imageThumbnail.getExtension();
+    imageThumbnail.write(smallfile);
+    await mintIt(instance.finalHash);
+  });
+}
+
+function watcherGeneral() {
+  watcher
+    .on("add", thisPath => log(`File ${thisPath} has been added`))
+    .on("change", thisPath => log(`File ${thisPath} has been changed`))
+    .on("unlink", thisPath => log(`File ${thisPath} has been removed`));
 }
